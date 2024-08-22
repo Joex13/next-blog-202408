4658d4e5185d3024c7a76c0dd27485c841e456db
Joex13 <aokijo0113p@gmail.com>
2024-08-23 00:40:11 +0900

    change: navタグ内の実装を変更

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,23 +1,23 @@
+import {
+  NavigationMenu,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu"
 import ModeToggler from "@/features/ModeToggler"
-import Link from "next/link"
 
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 p-4 w-full flex justify-between">
+      <NavigationMenu>
+        <NavigationMenuList className="flex gap-2">
+          <NavigationMenuLink href="/">HOME</NavigationMenuLink>
+          <NavigationMenuLink href="/test">TEST</NavigationMenuLink>
+          <NavigationMenuLink href="/matsuokashuzo">
+            松岡修造
+          </NavigationMenuLink>
+        </NavigationMenuList>
+      </NavigationMenu>
       <ModeToggler />
-      <nav>
-        <ul className="flex gap-2 h-full items-center">
-          <li>
-            <Link href="/">HOME</Link>
-          </li>
-          <li>
-            <Link href="test">TEST</Link>
-          </li>
-          <li>
-            <Link href="matsuokashuzo">ERROR</Link>
-          </li>
-        </ul>
-      </nav>
     </header>
   )
 }
