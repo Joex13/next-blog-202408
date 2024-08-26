ece8589a20abf0e910cbfb2998069dbd6b87e106
Joex13 <aokijo0113p@gmail.com>
2024-08-26 15:26:46 +0900

    refactor: map関数に置き換え

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -5,14 +5,18 @@ import {
 } from "@/components/ui/navigation-menu"
 import { ModeToggle } from "@/features/ModeToggle"
 
+const routes = ["HOME", "AUTHORS", "CONTACT"]
+
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 p-4 w-full flex justify-between">
       <NavigationMenu>
         <NavigationMenuList className="flex gap-2">
-          <NavigationMenuLink href="/">HOME</NavigationMenuLink>
-          <NavigationMenuLink href="/test">TEST</NavigationMenuLink>
-          <NavigationMenuLink href="/matsuokashuzo">えらー</NavigationMenuLink>
+          {routes.map((route) => (
+            <NavigationMenuLink href={route === "HOME" ? "/" : route}>
+              {route}
+            </NavigationMenuLink>
+          ))}
         </NavigationMenuList>
       </NavigationMenu>
       <ModeToggle />
