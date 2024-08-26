f44997b459e937c4dffac165bea7d870847c747e
Joex13 <aokijo0113p@gmail.com>
2024-08-26 15:38:10 +0900

    feat: 404ページを実装

---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
+
+export default function NOTFOUND() {
+  const router = useRouter()
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/")
+    }, 5000)
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <>
+      <h1>404ページです。</h1>
+      <h2>5秒後にホームへリダイレクトします。</h2>
+    </>
+  )
+}
